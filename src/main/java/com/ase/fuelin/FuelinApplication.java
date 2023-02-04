c8f92d6734306d5b0254cd65df33b952add79b3d
Vidushan Wijesinghe <wijesinghe.vidu@gmail.com>
2023-02-04 10:23:17 +0530

    Initial Commit

---
+++ src/main/java/com/ase/fuelin/FuelinApplication.java
@@ -0,0 +1,13 @@
+package com.ase.fuelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ase/fuelin/FuelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ase.fuelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
